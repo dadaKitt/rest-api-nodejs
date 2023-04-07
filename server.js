72abb7224e0c605d22d143e546dd38d9f23ebeb2
Kittichai Raksawong <93074696+dadaKitt@users.noreply.github.com>
2023-04-07 19:19:34 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const mysqli = require('mysql2')
+const mysql2 = require('mysql2')
 const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const connection = mysqli.createConnection({
+const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
@@ -14,16 +14,17 @@ const connection = mysqli.createConnection({
 
 
 app.post('/users', async(req, res) => {
-    connection.execute(
-        'SELECT * FROM users',
-        function(err, results, fields){
-            console.log(results)
-            console.log(fields)
-            return false
-        } 
-    );
+    res.json(req.body)
 })
 
+connection.execute(
+    'SELECT * FROM users',
+    function(err, results, fields){
+        console.log(results)
+        console.log(fields)
+        return false
+    } 
+);
 
 app.listen(port, () => {
   console.log(`Server run on port: ${port}`)
